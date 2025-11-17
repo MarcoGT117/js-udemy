ee9cb9c534368675ebbf5d0bd39c54bd7f863123
Marco Gonzalez <marco.g.gleztorres@gmail.com>
2025-11-17 17:51:52 -0600

    Update palindromo.js

--- palindromo.js
+++ palindromo.js
@@ -1,17 +1,19 @@
 function palindromo(palabra) {
+    
     let palabraArr = Array.from(palabra);
     let palabraArrInvertida = [];
     //El metodo map se aplica sobre cada elemento de palabraArr, este aplicara la funcion flecha para realizar
     //en cada elemento del array palabraArr (que ahora sera letra) un unshift y agregarlo al principio del array
     //palabraArrInvertida
-    palabraArr.map(letra=>palabraArrInvertida.unshift(letra));
-    console.log(palabraArr)
+    palabraArr.map(letra=>palabraArrInvertida.push(letra));
+    console.log(palabraArr.length);
+    console.log(palabraArrInvertida)
     let count = 0;
 
-    for(let i= 0; i > palabraArr.length; i++){
-        for (let j = 0 ;j > pabraArrInvertida.length; j++) {
+    for(let i= 0; i >= palabraArr.length; i++){
+        for (let j = 0 ;j >= palabraArrInvertida.length; j++) {
             if(palabraArr[i] === palabraArrInvertida[j]){
-                count++;
+                count= count + 1;
             };
         }
     }
@@ -26,5 +28,5 @@ function palindromo(palabra) {
 let test = "hola";
 palindromo(test); // -> No es un palindromo
 
-let test2 = "anitalavalatina";
+let test2 = "anilina";
 palindromo(test2); // -> Es un palindromo
